94a1456c9fa52561b7677233ec5479cdc764cd93
doer <e.yarmash@gmail.com>
2013-05-15 01:39:08 +0300

    Use recursion & memoization in the solution to the problem 31

--- 031/031.py
+++ 031/031.py
@@ -1,54 +1,20 @@
 #!/usr/bin/python2
 
-res = 0
+from projecteuler import memoize
 
-for c1 in [1*x for x in range(201)]:
-    s = c1
-    if s >= 200:
-        if s == 200: res += 1
-        break
+coins = [200, 100, 50, 20, 10, 5, 2, 1]
 
-    for c2 in [2*x for x in range(101)]:
-        s = c1+c2
-        if s >= 200:
-            if s == 200: res += 1
-            break
+@memoize
+def ways(amount, coin):
 
-        for c5 in [5*x for x in range(41)]:
-            s = c1+c2+c5
-            if s >= 200:
-                if s == 200: res += 1
-                break
+    if coin == 7 or amount == 0:
+        return 1
 
-            for c10 in [10*x for x in range(21)]:
-                s = c1+c2+c5+c10
-                if s >= 200:
-                    if s == 200: res += 1
-                    break
+    count = 0
 
-                for c20 in [20*x for x in range(11)]:
-                    s = c1+c2+c5+c10+c20
-                    if s >= 200:
-                        if s == 200: res += 1
-                        break
+    while amount >= 0:
+        count += ways(amount, coin+1)
+        amount -= coins[coin]
+    return count
 
-                    for c50 in [50*x for x in range(5)]:
-                        s = c1+c2+c5+c10+c20+c50
-                        if s >= 200:
-                            if s == 200: res += 1
-                            break
-
-                        for c100 in [100*x for x in range(3)]:
-                            s = c1+c2+c5+c10+c20+c50+c100
-                            if s >= 200:
-                                if s == 200: res += 1
-                                break
-
-                            for c200 in [200*x for x in range(2)]:
-                                s = c1+c2+c5+c10+c20+c50+c100+c200
-                                if s >= 200:
-                                    if s == 200: res += 1
-                                    break
-
-
-print res
+print ways(200, 0)

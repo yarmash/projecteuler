8ce8f7efe25119bbd2c438d7b7d17a4d5659ea1a
doer <e.yarmash@gmail.com>
2013-05-19 01:50:32 +0300

    Fix logic (not all legitimate numbers tested)

--- 036/036.py
+++ 036/036.py
@@ -2,20 +2,23 @@
 
 from projecteuler import is_palindrome
 
-# generate palindromes in base 2
-def palindromes(lim):
-    i = 1
+# generate a palindrome in base 2
+def mk_palindrome(n, oddlength):
+    r = n
+    if oddlength: n >>= 1
+    while n:
+        r = (r << 1) + (n & 1)
+        n >>= 1
+    return r
 
-    while True:
-        for n in (i>>1, i): # generate xyz+yx and xyz+zyx
-            p = i
-            while n:
-                p = (p << 1) + (n & 1)
-                n >>= 1
-            if p >= lim: return
+def palindromes(oddlength, lim=1000000):
+    i = 1
+    p = mk_palindrome(i, oddlength)
+    while p < lim:
+        if is_palindrome(p, 10):
             yield p
         i += 1
+        p = mk_palindrome(i, oddlength)
 
-lim = 1000000
-
-print sum([ p for p in palindromes(lim) if is_palindrome(p, 10) ]);
+# sum of the palindromes of the form xyz+yx and xyz+zyx
+print sum(p for p in palindromes(True)) + sum(p for p in palindromes(False))

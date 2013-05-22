28ba52ede32bcae64c0a699035ccb6f117b767f1
doer <e.yarmash@gmail.com>
2013-05-22 14:52:11 +0300

    Minor optimization

--- 041/041.py
+++ 041/041.py
@@ -4,13 +4,15 @@ from itertools import permutations
 from projecteuler import is_prime, is_pandigital
 
 
-digits = "987654321"
+digits = (7, 6, 5, 4, 3, 2, 1)
 
 def largest_prime():
-    for i in [7, 4]: # only 7 or 4 digit numbers need to be considered (in other cases the sum is divisible by 3)
-        for p in permutations(digits[9-i:], i):
-            p = int("".join(p))
+    for ndigits in [7, 4]: # only 7 or 4 digit numbers need to be considered (in other cases the sum is divisible by 3)
+        for p in permutations(digits[7-ndigits:], ndigits):
+            n = 0
+            for i in range(ndigits):
+                n += 10**(ndigits-i-1)*p[i]
 
-            if is_pandigital(p, i) and is_prime(p):
-                return p
+            if is_pandigital(n, ndigits) and is_prime(n):
+                return n
 print largest_prime()

590674f1d9f1e8d0f718a725660d660e55da1ac7
doer <e.yarmash@gmail.com>
2013-05-25 23:34:44 +0300

    Minor optimization

--- 047/047.py
+++ 047/047.py
@@ -5,12 +5,12 @@ from math import sqrt
 lim = 1000000
 sieve = [0]*lim
 
-for i in range(2, int(sqrt(lim))+1):
+for i in xrange(2, int(sqrt(lim))+1):
     if not sieve[i]: # prime
-        for k in range(i+i, lim-lim%i, i):
+        for k in xrange(i+i, lim, i):
             sieve[k] += 1
-
-for i in range(2, len(sieve)-5):
-    if sieve[i:i+4] == [4,4,4,4]:
+l = [4]*4
+for i in xrange(2, len(sieve)-4):
+    if sieve[i:i+4] == l:
         print i
         break

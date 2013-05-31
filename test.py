9ba8b76e3e41ddbe53c92cf39ffc8cb9a3da1edf
doer <e.yarmash@gmail.com>
2013-05-31 19:12:48 +0300

    Add a testing script in Python

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+import os
+import re
+import sys
+
+bindir = os.path.dirname(__file__)
+
+dirs = sorted([ d for d in os.listdir(bindir) if re.match('[0-9]{3}$', d) ])
+
+for d in dirs:
+    sys.path.insert(0, os.path.join(bindir, d))
+    mod =  __import__("problem"+str(int(d)))
+    answer = open(os.path.join(bindir, d, "answer")).read().rstrip()
+    print d+" ",
+    if answer == str(mod.main()):
+        print "OK"
+    else:
+        print "FAIL"
+    sys.path.pop(0)

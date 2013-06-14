f6a98a2aa10d14d55c95f85a352fefcb9fb961ff
doer <e.yarmash@gmail.com>
2013-06-14 10:35:09 +0300

    Add a solution to the problem 59

---
+++ 059/problem59.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+
+import os
+
+def main():
+    datafile = os.path.join(os.path.dirname(__file__), "cipher1.txt")
+    codes = eval(open(datafile).read())
+
+    frequency = [{}, {}, {}] # code frequency for each character of the key
+
+    for i, code in enumerate(codes):
+        frequency[i%3][code] = frequency[i%3].get(code, 0) + 1
+
+    # In English, the space (32) is the most frequent character
+    key = map(lambda f: 32^max(f.keys(), key=lambda x: f[x]), frequency)
+
+    return sum((k^key[i])*v for i, f in enumerate(frequency) for k, v in f.iteritems())
+
+if __name__ == "__main__":
+    print main()

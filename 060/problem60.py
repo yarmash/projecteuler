96e4c1335dc34ad7de2912a0612f40b0a0f7fcdd
doer <e.yarmash@gmail.com>
2013-06-16 23:23:19 +0300

    Use loops and sets intersection instead of recursion

--- 060/problem60.py
+++ 060/problem60.py
@@ -4,32 +4,26 @@ from projecteuler import prime_sieve
 from miller_rabin import miller_rabin as is_prime
 
 def main():
-    lim = 8500
+    lim = 8400
     primes = prime_sieve(lim)
+    del primes[2] # delete 5 and 2
+    del primes[0]
 
     # store possible pairs for each prime
-    candidates = [None]*len(primes)
-    for i in xrange(1, len(primes)):
-        candidates[i] = set(j for j in xrange(i+1, len(primes)) if is_prime(int(str(primes[i])+str(primes[j]))) and is_prime(int(str(primes[j])+str(primes[i]))))
+    candidates = [ set(j for j in xrange(i+1, len(primes)) if is_prime(int(str(primes[i])+str(primes[j]))) and is_prime(int(str(primes[j])+str(primes[i]))))
+        for i in xrange(len(primes)) ]
 
-    quintuplets = []
+    res = float("Inf")
 
-    def find_quints(p):
-        if len(p) == 5:
-            quintuplets.append(sum(primes[i] for i in p))
-            return
-
-        for i in xrange(p[-1]+1, len(primes)):
-            for j in p:
-                if not i in candidates[j]:
-                    break
-            else:
-                find_quints(p+[i])
-
-    for i in xrange(1, len(primes)-4):
-        find_quints([i])
-
-    return min(quintuplets)
+    for i in xrange(len(primes)-4):
+        for j in candidates[i]:
+            for k in candidates[i] & candidates[j]:
+                for l in candidates[i] & candidates[j] & candidates[k]:
+                    for m in candidates[i] & candidates[j] & candidates[k] & candidates[l]:
+                        s = sum(primes[x] for x in (i, j, k, l, m))
+                        if s < res:
+                            res = s
+    return res
 
 if __name__ == "__main__":
     print main()

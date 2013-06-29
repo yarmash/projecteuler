e2024abaca832b3b546b52349b3b15ddd85376ed
doer <e.yarmash@gmail.com>
2013-06-29 18:23:33 +0300

    Add a solution to the problem 63

---
+++ 063/problem63.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+
+"""Problem 63: Powerful digit counts"""
+
+def main():
+    cnt = 0
+
+    # if num >= 10, num^n has more than n digits for each n
+    for num in xrange(1, 10):
+        power = 1
+
+        # if num^n has less than n digits, then num^k has less than k digits for each k > n
+        while len(str(num**power)) == power:
+            cnt += 1
+            power += 1
+
+    return cnt
+
+
+if __name__ == "__main__":
+    print main()

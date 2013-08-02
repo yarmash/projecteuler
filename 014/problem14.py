1b97530c367cda9c2e65240f1536ae7d4e41a564
doer <e.yarmash@gmail.com>
2013-08-02 11:12:24 +0300

    Minor optimization

--- 014/problem14.py
+++ 014/problem14.py
@@ -8,14 +8,12 @@ def main():
     num = 0
 
     def get_chain_length(n):
-        if n in cache:
-            return cache[n]
+        if n == 2:
+            return 2
 
-        if n == 1:
-            cache[n] = length = 1
-        else:
-            cache[n] = length = 1 + get_chain_length(3*n+1 if n&1 else n>>1)
-        return length
+        if not n in cache:
+            cache[n] = 2 + get_chain_length((3*n+1) >> 1) if n&1 else 1 + get_chain_length(n>>1)
+        return cache[n]
 
     for i in xrange(2, 1000000):
         length = get_chain_length(i)

0c4c3748bc8a6891f7ff883742f0d95fdba9ad6d
doer <e.yarmash@gmail.com>
2013-09-08 18:07:28 +0300

    Use defaultdict

--- 059/problem59.py
+++ 059/problem59.py
@@ -3,18 +3,20 @@
 """Problem 59: XOR decryption"""
 
 import os
+from collections import defaultdict
 
 def main():
     datafile = os.path.join(os.path.dirname(__file__), "cipher1.txt")
     codes = eval(open(datafile).read())
 
-    frequency = [{}, {}, {}] # code frequency for each character of the key
+    # code frequency for each character of the key
+    frequency = [ defaultdict(int) for i in xrange(3) ]
 
     for i, code in enumerate(codes):
-        frequency[i%3][code] = frequency[i%3].get(code, 0) + 1
+        frequency[i % 3][code] += 1
 
     # In English, the space (32) is the most frequent character
-    key = [ 32^max(f.keys(), key=lambda x: f[x]) for f in frequency ]
+    key = [ 32^max(f.keys(), key=f.get) for f in frequency ]
 
     return sum((k^key[i])*v for i, f in enumerate(frequency) for k, v in f.iteritems())
 

2236803eec62dea0d059cccbdf14c48e9875f471
doer <e.yarmash@gmail.com>
2013-09-08 12:47:35 +0300

    OOP for the win

--- 070/problem70.py
+++ 070/problem70.py
@@ -3,6 +3,7 @@
 """Totient permutation"""
 
 from projecteuler import prime_sieve, is_permutation
+from bisect import bisect_left
 from Queue import PriorityQueue
 
 def main():
@@ -10,44 +11,46 @@ def main():
     # cannot be a permutation of each other
 
     lim = 10**7
-    primes = prime_sieve(5000)
+    primes = prime_sieve(4000)
+    last_idx = len(primes)-1
+    # the max prime <= sqrt(10**7) is 3137
+    middle_idx = bisect_left(primes, 3137)
 
-    def gen_product(start):
-        for i in xrange(len(primes)-1, start, -1):
-            if primes[start]*primes[i] <= lim:
-                yield primes[start], primes[i]
+    def candidate_primes(low_idx):
+        for high_idx in xrange(last_idx, low_idx, -1):
+            if primes[low_idx]*primes[high_idx] <= lim:
+                yield high_idx
 
+    class Number():
+        """Number which is the product of two primes"""
 
-    def n_by_phi(p1, p2):
-        return float(p1*p2)/((p1-1)*(p2-1))
+        def __init__(self, low_idx, gen):
+            self.low_idx = low_idx
+            self.gen = gen
+            low = primes[self.low_idx]
+            high = primes[self.gen.next()]
+            self.number = low*high
+            self.phi = (low-1)*(high-1)
 
+        def __lt__(self, other):
+            return self.number*other.phi < other.number*self.phi
 
-    def sorted_by_ratio():
-        queue = PriorityQueue()
+    queue = PriorityQueue()
 
-        # the max prime <= sqrt(10**7) is 3137, which has the index of 445
-        middle_idx, middle = 445, 3137
+    for low_idx in xrange(middle_idx, -1, -1):
+        gen = candidate_primes(low_idx)
+        queue.put(Number(low_idx, gen))
 
-        for i in xrange(middle_idx, -1, -1):
-            it = gen_product(i)
-            pair = it.next()
-            queue.put((n_by_phi(*pair), pair, it))
+    while not queue.empty():
+        number = queue.get()
 
+        if is_permutation(number.number, number.phi):
+            return number.number
 
-        while not queue.empty():
-            factor, pair, it = queue.get()
-            yield pair
-
-            try:
-                pair = it.next()
-                queue.put((n_by_phi(*pair), pair, it))
-            except StopIteration:
-                pass
-
-    for p1, p2 in sorted_by_ratio():
-        if is_permutation(p1*p2, (p1-1)*(p2-1)):
-            return p1*p2
-
+        try:
+            queue.put(Number(number.low_idx, number.gen))
+        except StopIteration:
+            pass
 
 if __name__ == "__main__":
     print main()

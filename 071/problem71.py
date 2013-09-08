a71b6028ae2ad4085ed9d01bade5f4b6a9db2c26
doer <e.yarmash@gmail.com>
2013-09-09 01:02:57 +0300

    Add a faster solution to the problem 71

--- 071/problem71.py
+++ 071/problem71.py
@@ -2,17 +2,16 @@
 
 """Ordered fractions"""
 
-from fractions import Fraction
-
 def main():
-    lim = 1000000
+    # http://en.wikipedia.org/wiki/Farey_sequence
+    a, b, c, d = 0, 1000000, 3, 7
 
-    def get_fraction(den):
-        """Returns the largest fraction below 3/7 with the given nummerator"""
-        num = (den*3-1)/7
-        return Fraction(num, den)
+    while True:
+        a = (b*c-1)/d
+        if b*c - a*d == 1:
+            return a
+        b -= 1
 
-    return max(get_fraction(i) for i in xrange(2, lim+1)).numerator
 
 if __name__ == "__main__":
     print main()

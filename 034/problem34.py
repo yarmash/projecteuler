ce0e5ea9d515cbb0b5e4a38421bf60932607af19
doer <e.yarmash@gmail.com>
2013-09-15 15:33:17 +0300

    Hardcode the factorials

--- 034/problem34.py
+++ 034/problem34.py
@@ -2,18 +2,26 @@
 
 """Problem 34: Digit factorials"""
 
-from projecteuler import factorials_table
-
 def main():
-    # precalculate factorials
-    f = factorials_table(9)
+    factorials = [
+        1,
+        1,
+        2,
+        6,
+        24,
+        120,
+        720,
+        5040,
+        40320,
+        362880,
+    ]
     res = 0
 
     for n in xrange(3, 40586): # For the proper upper bound see http://en.wikipedia.org/wiki/Factorion#Upper_bound
         s = 0
         k = n
         while k > 0:
-            s += f[k % 10]
+            s += factorials[k % 10]
             k /= 10
 
         if n == s:

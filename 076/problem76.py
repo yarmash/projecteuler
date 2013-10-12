fc7065cc09678d69f523534d622b802cd36ec635
doer <e.yarmash@gmail.com>
2013-10-12 13:44:42 +0300

    Same algorithm, but more compact

--- 076/problem76.py
+++ 076/problem76.py
@@ -8,20 +8,14 @@ def main():
 
     @memoize
     def count(k, n):
-
-        if k == n or n == 0: return 1
-        if k > n: return 0
-
-        cnt = 0
-
-        while n >= 0:
-            cnt += count(k+1, n)
-            n -= k
-
-        return cnt
+        """Returns the number of summations of n using numbers >= k"""
+        if k == n:
+            return 1
+        if k > n:
+            return 0
+        return count(k, n-k) + count(k+1, n)
 
     return count(1, 100) - 1
-
 
 if __name__ == "__main__":
     print main()

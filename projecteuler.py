96608b770ca5389ce1824956f110057803085a40
doer <e.yarmash@gmail.com>
2014-02-17 18:56:05 +0300

    Refactor the is_prime and prime_factors functions

--- projecteuler.py
+++ projecteuler.py
@@ -21,25 +21,26 @@ def memoize(func):
 
 
 def is_prime(n):
-    if n <= 1:
-        return False
-    elif n < 4:
-        return True # 2 and 3 are prime
-    elif n % 2 == 0:
-        return False
-    elif n < 9:
-        return True # we have already excluded 4, 6 and 8
-    elif n % 3 == 0:
-        return False
-    else:
-        limit = int(sqrt(n))
-        k = 5
-        while k <= limit: # check through all the numbers of the form 6k ± 1
-            if n % k == 0 or n % (k+2) == 0:
-                return False
-            k += 6
+    """Determine whether a number is prime using trial division"""
 
+    if n < 2:
+        return False
+    if n == 2 or n == 3:
         return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    max_divisor = int(sqrt(n))
+
+    # all primes are of the form 6k ± 1, with the exception of 2 and 3
+    divisor = 5
+
+    while divisor <= max_divisor:
+        if n % divisor == 0 or n % (divisor+2) == 0:
+            return False
+        divisor += 6
+
+    return True
 
 
 # returns the prime numbers <= limit
@@ -79,22 +80,20 @@ def prime_sieve_lazy():
         n += 2
 
 
-# returns prime factors of an integer
 def prime_factors(n):
+    """Return prime factors of an integer"""
+
     primes = prime_sieve(int(sqrt(n)))
     factors = []
 
-    for p in primes:
-        if p*p > n:
-            break
+    for prime in primes:
+        exponent = 0
+        while n % prime == 0:
+            exponent += 1
+            n //= prime
 
-        e = 0
-        while n % p == 0:
-            e += 1
-            n //= p
-
-        if e > 0:
-            factors.append((p, e))
+        if exponent:
+            factors.append((prime, exponent))
 
     if n > 1:
         factors.append((n, 1))

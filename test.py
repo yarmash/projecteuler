4b4b777793f3341f1ae885cebae55ee54da15011
doer <e.yarmash@gmail.com>
2014-03-17 01:29:39 +0300

    Show 5 slowest solutions at the end

--- test.py
+++ test.py
@@ -5,23 +5,32 @@ import sys
 import time
 
 bindir = os.path.dirname(__file__)
+dirs = sorted(d for d in os.listdir(bindir) if d.isdigit())
+total_time = 0
+runs = {}
 
-dirs = sorted([ d for d in os.listdir(bindir) if d.isdigit() ])
-
-start = time.clock()
-cnt = 0
 for d in dirs:
-    cnt += 1
-    sys.path.insert(0, os.path.join(bindir, d))
-    mod =  __import__("problem"+str(int(d)))
-    sys.path.pop(0)
+    sys.path.append(os.path.join(bindir, d))
+    mod = __import__("problem"+str(int(d)))
+    sys.path.pop()
     answer = open(os.path.join(bindir, d, "answer")).read().rstrip()
     sys.stdout.write(d+"  ")
     sys.stdout.flush()
     begin = time.clock()
+
     if answer == str(mod.main()):
-        print("OK ({0:.2f}s)".format(time.clock() - begin))
+        t = time.clock() - begin
+        total_time += t
+        runs[d] = t
+        print("OK ({0:.2f}s)".format(t))
     else:
         print("FAIL")
 
-print("Total: {0} problems in {1:.2f}s".format(cnt, time.clock()-start))
+print("Total: {0} problems in {1:.2f}s".format(len(dirs), total_time))
+
+slowpokes = sorted(((v, k) for k, v in runs.items()), reverse=True)[0:5]
+
+print("\nSlowest solutions:")
+
+for s in slowpokes:
+    print("{1}  {0:.2f}s".format(*s))

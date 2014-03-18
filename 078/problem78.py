4572d153d1f4541129810c64819cab5038418672
doer <e.yarmash@gmail.com>
2014-03-18 12:16:02 +0300

    Use the partition formula with bounds

--- 078/problem78.py
+++ 078/problem78.py
@@ -2,32 +2,32 @@
 
 """Problem 78: Coin partitions"""
 
+from math import sqrt, floor, ceil
+from itertools import count
+
+
+# http://mathworld.wolfram.com/PartitionFunctionP.html
 def main():
-    partitions = [1]
-    n = 1
+    # The first few values of the partition function (starting with p(0)=1)
+    partitions = [1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42]
 
-    while True:
-        t = 1
-        k = 1
-        p = 0
-        sign = 1
+    for n in count(len(partitions)):
+        lower_bound = ceil(-(sqrt(24*n+1)+1)/6)
+        upper_bound = floor((sqrt(24*n+1)-1)/6)
 
-        while t > 0:
-            t = n - (k*(3*k+1) >> 1)
-            if t >= 0:
-                p += sign*partitions[t]
-            t = n - (k*(3*k-1) >> 1)
-            if t >= 0:
-                p += sign*partitions[t]
-            k += 1
+        sum_ = 0
+
+        sign = -1 if lower_bound & 1 else 1
+
+        for k in range(lower_bound, upper_bound+1):
+            if k:
+                sum_ -= sign * partitions[n - (k*(3*k+1) >> 1)]
             sign = -sign
 
-        if not p % 1000000:
+        if not sum_ % 1000000:
             return n
 
-        n += 1
-        partitions.append(p)
-
+        partitions.append(sum_)
 
 if __name__ == "__main__":
     print(main())

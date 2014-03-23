c8d8c4460ad3c03eb09134221bc0d6d3ec624132
doer <e.yarmash@gmail.com>
2014-03-24 01:34:05 +0300

    Optimize the solution

--- 085/problem85.py
+++ 085/problem85.py
@@ -2,51 +2,37 @@
 
 """Problem 85: Counting rectangles"""
 
-
-import sys
-sys.setrecursionlimit(10000)
-
-
 def main():
-    cache = {}
 
-    def expand(grid, rectangles, next_r, next_c):
+    # A rectangle on a grid is bounded by two vertical grid lines and two
+    # horizontal grid lines. In an a x b grid the number of rectangles is
+    # C(a+1,2)*C(b+1,2) = a*(a+1)*b*(b+1)/4
 
-        if (next_r, next_c) in cache:
-            return cache[(next_r, next_c)]
-        elif (next_c, next_r) in cache:
-            return cache[(next_c, next_r)]
+    N = 2000000
+    diff = N-1
+    area = 0
+    a = 1
 
-        if sum(rectangles.values()) >= 2000000:
-            return
+    while True:
+        b = 0
+        nrectangles = 0
+        while nrectangles < N:
+            b += 1
+            nrectangles = a*(a+1)*b*(b+1) >> 2
 
-        if next_r > grid[0]:  # moving down
-            # Update counters for existing rectangles.
-            for k, v in rectangles.items():
-                rectangles[k] += next_c - k[1] + 1
+        if nrectangles - N < diff:
+            diff = nrectangles - N
+            area = a*b
 
-            # Add new rectangles.
-            for col in range(1, next_c+1):
-                rectangles[next_r, col] = col
-        else:  # moving right
-            for k, v in rectangles.items():
-                rectangles[k] += next_r - k[0] + 1
+        if N - nrectangles*(b-1)//(b+1) < diff:
+            diff = N - nrectangles*(b-1)//(b+1)
+            area = a*(b-1)
 
-            for row in range(1, next_r+1):
-                rectangles[row, next_c] = row
+        if a >= b:
+            break
+        a += 1
 
-        grid = [next_r, next_c]
-        cache[(next_r, next_c)] = sum(rectangles.values())
-
-        expand(list(grid), rectangles.copy(), next_r, next_c+1)
-        expand(list(grid), rectangles.copy(), next_r + 1, next_c)
-
-    rectangles = {(1, 1): 1}
-    expand([1, 1], rectangles, 1, 2)
-
-    for k in sorted(cache, key=cache.get, reverse=True):
-        if cache[k] < 2000000:
-            return k[0]*k[1]
+    return area
 
 
 if __name__ == "__main__":

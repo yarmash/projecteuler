79bcc47e4adf5c7562a99407180d28542d30a49a
doer <e.yarmash@gmail.com>
2014-09-03 19:26:19 +0300

    Improve the readability a bit

--- 087/problem87.py
+++ 087/problem87.py
@@ -7,18 +7,19 @@ from projecteuler import prime_sieve
 
 def main():
     lim = 50000000
-    lim_square = int(pow(lim-8-16, 1/2))
-    lim_cube = int(pow(lim-4-16, 1/3))
-    lim_fourth = int(pow(lim-4-8, 1/4))
+    lim_square = int((lim - 2**3 - 2**4) ** (1/2))
+    lim_cube = int((lim - 2**2 - 2**4) ** (1/3))
+    lim_fourth = int((lim - 2**2 - 2**3) ** (1/4))
 
     primes = prime_sieve(lim_square)
 
-    squares = [pow(p, 2) for p in primes]
-    cubes = [pow(p, 3) for p in primes if p <= lim_cube]
-    fourths = [pow(p, 4) for p in primes if p <= lim_fourth]
+    squares = [p**2 for p in primes]
+    cubes = [p**3 for p in primes if p <= lim_cube]
+    fourths = [p**4 for p in primes if p <= lim_fourth]
 
     numbers = set([
-        square + cube + fourth for fourth in fourths
+        square + cube + fourth
+            for fourth in fourths
             for cube in cubes if fourth + cube < lim
             for square in squares if fourth + cube + square < lim
     ])

29e00071b30c26b11181a9bd0f701e55d2e2a50a
doer <e.yarmash@gmail.com>
2014-09-23 20:37:19 +0300

    Optimize the algorithm using recursion

--- 088/problem88.py
+++ 088/problem88.py
@@ -2,32 +2,31 @@
 
 """Problem 88: Product-sum numbers"""
 
-from functools import reduce
-from operator import mul
-from itertools import count
-from math import ceil, sqrt
-
 
 def main():
     # the minimal product-sum for some k is between k and 2k
+    # 2k is a guaranteed solution as the set {2, k} can be padded with k-2 ones
 
-    min_product_sums = [float("inf")]*12001
-    min_product_sums[0:2] = 0, 0
+    K = 12000 # maximum k
+    N = K*2   # limit for product sum
 
-    factors = [[i] for i in range(2, ceil(sqrt(24000)))]
+    min_product_sums = [N]*(K+1)
+    min_product_sums[:2] = 0, 0
 
-    # generate combinations of factors in nondecreasing order
-    while factors:
-        f = factors.pop()
-        product_sum = reduce(mul, f)
-        k = (product_sum-sum(f)) + len(f)
-        if k <= 12000 and product_sum < min_product_sums[k]:
-            min_product_sums[k] = product_sum
+    def search(product, sum_, num_factors, max_factor):
+        k = product - sum_ + num_factors
 
-        for i in count(f[-1]):
-            if product_sum*i > 24000:
-                break
-            factors.append(f + [i])
+        if k > K:
+            return
+
+        if product < min_product_sums[k]:
+            min_product_sums[k] = product
+
+        for factor in range(max_factor, N//product+1):
+            search(product*factor, sum_+factor, num_factors+1, factor)
+
+    for factor in range(2, int(N**0.5)+1):
+        search(factor, factor, 1, factor)
 
     return sum(set(min_product_sums))
 

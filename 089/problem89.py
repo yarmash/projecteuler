13f8293e690d47e1e355e19eae6cfdbc2a9f9fe6
doer <e.yarmash@gmail.com>
2014-10-02 16:37:04 +0300

    Optimize the solution
    Use the fact that all the Roman numerals in the file contain no more than four consecutive
    identical units and calculate the result on the fly.

--- 089/problem89.py
+++ 089/problem89.py
@@ -4,60 +4,31 @@
 
 from projecteuler import open_data_file
 
-VALUES = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-
-    # subtractive combinations
-    "IV": 4,
-    "IX": 9,
-    "XL": 40,
-    "XC": 90,
-    "CD": 400,
-    "CM": 900,
-}
-
-NUMERALS = sorted(VALUES, key=VALUES.get, reverse=True)
-
-
-def roman_to_arabic(roman):
-    """Convert a roman numeral to arabic number"""
-    arabic = 0
-
-    for i in range(len(roman)-1):
-        if VALUES[roman[i]] < VALUES[roman[i+1]]:
-            arabic -= VALUES[roman[i]]
-        else:
-            arabic += VALUES[roman[i]]
-    arabic += VALUES[roman[-1]]
-
-    return arabic
-
-
-def arabic_to_roman(arabic):
-    """Convert an arabic number to roman numeral in the minimal form"""
-    numerals = []
-
-    for numeral in NUMERALS:
-        while arabic >= VALUES[numeral]:
-            arabic -= VALUES[numeral]
-            numerals.append(numeral)
-    return "".join(numerals)
-
 
 def main():
-    saved_chars = 0
+    chars_saved = 0
+
+    # From the problem definition:
+    # You can assume that all the Roman numerals in the file contain no more
+    # than four consecutive identical units.
 
     for line in open_data_file("roman.txt"):
-        line = line.rstrip()
-        saved_chars += len(line) - len(arabic_to_roman(roman_to_arabic(line)))
+        if "VIIII" in line:
+            chars_saved += 3  # VIIII => IX
+        elif "IIII" in line:
+            chars_saved += 2  # IIII => IV
 
-    return saved_chars
+        if "LXXXX" in line:
+            chars_saved += 3  # LXXXX => XC
+        elif "XXXX" in line:
+            chars_saved += 2  # XXXX => XL
+
+        if "DCCCC" in line:
+            chars_saved += 3  # DCCCC => CM
+        elif "CCCC" in line:
+            chars_saved += 2  # CCCC => CD
+
+    return chars_saved
 
 
 if __name__ == "__main__":

71f128a8b48de2f0b760815b5a2896f163a74716
doer <e.yarmash@gmail.com>
2014-10-17 18:54:28 +0300

    Optimize the sum_squares function

--- 092/problem92.py
+++ 092/problem92.py
@@ -11,24 +11,18 @@ def main():
     @memoize
     def sum_squares(num):
         """Sums the squares of the digits in a number"""
-        if num == 1 or num == 89:
+        if num == 89 or num == 1 or num == 0:
             return num
-        s = 0
-        while num:
-            d = num % 10
-            s += d*d
-            num //= 10
-        return sum_squares(s)
+
+        # the number is limited by 9^2*7 == 567
+        a, b, c = num // 100, (num % 100) // 10, num % 10
+        return sum_squares(a*a + b*b + c*c)
 
     factorials = [factorial(x) for x in range(8)]
     numbers = 0
 
     for digits in combinations_with_replacement(range(10), 7):
-
-        number = digits[0]*1000000 + digits[1]*100000 + digits[2]*10000 + \
-            digits[3]*1000 + digits[4]*100 + digits[5]*10 + digits[6]
-
-        if number and sum_squares(number) == 89:
+        if sum_squares(sum([x*x for x in digits])) == 89:
             counts = [0]*10
             for digit in digits:
                 counts[digit] += 1

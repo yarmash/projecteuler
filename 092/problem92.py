f1d6325334097c6efcea98f94e885ec2752baeb4
doer <e.yarmash@gmail.com>
2014-10-18 22:00:32 +0300

    Optimize the solution
    Process numbers n <= 9^2*7 and calculate how many numbers below 10^7
    have the sum of the squares of their digits equal to n

--- 092/problem92.py
+++ 092/problem92.py
@@ -3,37 +3,28 @@
 """Problem 92: Square digit chains"""
 
 from projecteuler import memoize
-from itertools import combinations_with_replacement
-from math import factorial
-
 
 def main():
+
     @memoize
     def sum_squares(num):
         """Sums the squares of the digits in a number"""
-        if num == 89 or num == 1 or num == 0:
+        if num == 89 or num == 1:
             return num
 
-        # the number is limited by 9^2*7 == 567
         a, b, c = num // 100, (num % 100) // 10, num % 10
         return sum_squares(a*a + b*b + c*c)
 
-    factorials = [factorial(x) for x in range(8)]
-    numbers = 0
 
-    for digits in combinations_with_replacement(range(10), 7):
-        if sum_squares(sum([x*x for x in digits])) == 89:
-            counts = [0]*10
-            for digit in digits:
-                counts[digit] += 1
+    @memoize
+    def count_numbers(n, k):
+        """Calculate how many numbers have the sum of the squares equal to n"""
+        if n < 0: return 0
+        if n == k == 0: return 1
+        if k == 0: return 0
+        return sum([count_numbers(n - i*i, k - 1) for i in range(10)])
 
-            permutations = factorials[7]
-            for count in counts:
-                if count > 1:
-                    permutations //= factorials[count]
-            numbers += permutations
-
-    return numbers
+    return sum([count_numbers(num, 7) for num in range(2, 9**2*7+1) if sum_squares(num) == 89])
 
 if __name__ == "__main__":
     print(main())

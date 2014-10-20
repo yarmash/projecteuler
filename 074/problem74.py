45e3effcd72ae66dd9e3f6283f52441c2b655529
doer <e.yarmash@gmail.com>
2014-10-20 14:32:16 +0300

    Optimize the solution
    Check only unique combinations of digits and calculate permutations

--- 074/problem74.py
+++ 074/problem74.py
@@ -2,19 +2,17 @@
 
 """Problem 74: Digit factorial chains"""
 
+from itertools import combinations_with_replacement
+
 
 def main():
-    lim = 1000000
-    factorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
-    cache = [False]*(factorials[9]*6 + 1)
-    cache[0] = 2
+    factorials = (1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880)
+    cache = {0: 2, 1: 1, 145: 1, 169: 3, 871: 2, 872: 2, 69: 5, 78: 4, 540: 2}
 
     def get_chain_len(n, factorials=factorials, cache=cache):
         """Return chain length for a given number"""
-
-        if cache[n]:
+        if n in cache:
             return cache[n]
-
         chain = [n]
 
         while True:
@@ -24,12 +22,10 @@ def main():
                 fsum += factorials[n % 10]
                 n //= 10
 
-            if cache[fsum]:
+            if fsum in cache:
                 length = cache[fsum] + len(chain)
-
                 for i, v in enumerate(chain):
                     cache[v] = length - i
-
                 return length
 
             if fsum in chain:
@@ -37,14 +33,40 @@ def main():
                     if v == fsum and i > 0:
                         break
                     cache[v] = len(chain) - i
-
                 return len(chain)
 
             chain.append(fsum)
             n = fsum
 
-    return sum(1 for i in range(lim) if get_chain_len(i) == 60)
+    chains = 0
 
+    # TODO: this solution is not complete (e.g. combinations with trailing
+    # zeroes are not considered, as well as repeating permutations containing "1"s)
+    for digits in combinations_with_replacement(range(10), 6):
+        number = 0
+        for d in digits:
+            number = number*10 + d
+
+        if get_chain_len(number) == 60:
+            digits = [d for d in digits if d]  # skip leading zeroes
+            ndigits = len(digits)
+
+            # calculate the number of distinguishable permutations
+            counts = [0]*10
+            for digit in digits:
+                counts[digit] += 1
+
+            perms = factorials[ndigits]
+            for count in counts:
+                if count > 1:
+                    perms //= factorials[count]
+            chains += perms
+
+            # 1 and 0 are interchangable except in the leftmost part of the number
+            if counts[1]:
+                chains += factorials[ndigits] - counts[1]*factorials[ndigits-1]
+
+    return chains
 
 if __name__ == "__main__":
     print(main())

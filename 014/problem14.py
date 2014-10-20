8afb76808768934ffe7c41c8a90620036997b42d
doer <e.yarmash@gmail.com>
2014-10-20 21:43:29 +0300

    Optimize the solution
    Iterate only through the second half of the numbers in the range

--- 014/problem14.py
+++ 014/problem14.py
@@ -3,19 +3,18 @@
 """Problem 14: Longest Collatz sequence"""
 
 def main():
-    cache = {}
-    longest = 0
-    num = 0
 
-    def get_chain_length(n):
-        if n == 2:
-            return 2
-
+    def get_chain_length(n, cache={2: 2}):
         if not n in cache:
-            cache[n] = 2 + get_chain_length((3*n+1) >> 1) if n&1 else 1 + get_chain_length(n>>1)
+            cache[n] = 2 + get_chain_length((3*n+1)>>1) if n&1 else 1 + get_chain_length(n>>1)
         return cache[n]
 
-    for i in range(2, 1000000):
+    limit = 1000000
+    longest = num = 0
+
+    # Look only at the second half of the numbers in the range
+    # For every number n ≤ limit//2, 2n is also in the range
+    for i in range(limit//2, limit):
         length = get_chain_length(i)
 
         if length > longest:

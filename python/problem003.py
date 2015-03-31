ca2b83264ed281a0086dd9cc0c85618fbb8b899b
doer <e.yarmash@gmail.com>
2015-03-31 20:21:19 +0300

    Optimize the solution & improve style

--- python/problem003.py
+++ python/problem003.py
@@ -2,18 +2,21 @@
 
 """Problem 3: Largest prime factor"""
 
+
 def main():
     num = 600851475143
-    f = 3
+    max_factor = int(num**.5)
+    factor = last_factor = 3
 
-    while num > 1:
-        if (num % f == 0):
-            f2 = f
-        while (num % f == 0):
-            num /= f
-        f += 2
+    while num > 1 and factor <= max_factor:
+        if num % factor == 0:
+            last_factor = factor
+            while num % factor == 0:
+                num //= factor
+            max_factor = int(num**.5)
+        factor += 2
 
-    return f2
+    return last_factor if num == 1 else num
 
 if __name__ == "__main__":
     print(main())

fa5d2cabbea188d639d2bef7a9bf45bfc161a0ee
doer <e.yarmash@gmail.com>
2015-04-01 19:39:14 +0300

    Optimize the solution

--- python/problem093.py
+++ python/problem093.py
@@ -2,72 +2,53 @@
 
 """Problem 93: Arithmetic expressions"""
 
-from itertools import product, combinations, permutations, zip_longest
+from itertools import product, combinations, permutations
 from operator import add, sub, mul, truediv
 
 
-def calc_consecutive(seq):
-    """Return the number of consecutive integers 1 to n in the sequence"""
-    last = seq[0]
-    if last != 1:
-        return 0
-
-    for i in range(1, len(seq)):
-        if seq[i] != last + 1:
-            return last
-        last += 1
-    return last
+def calc_consecutive(targets):
+    """Return the number of consecutive integers 1 to n in a set"""
+    i = 0
+    while i+1 in targets:
+        i += 1
+    return i
 
 
 def main():
     max_consecutive = 0
-    answer = None
-    operations = [add, sub, mul, truediv]
+    operations = (add, sub, mul, truediv)
 
     for digits in combinations(range(1, 10), 4):
-        perms = list(permutations(digits, 4))
-
-        terms = []
-
-        for perm in perms:
-            for ops in product(operations, repeat=3):
-                ops = ops[:4]
-
-                term = []
-                for pair in zip_longest(perm, ops):
-                    if pair[1] is not None:
-                        term.extend(pair)
-                    else:
-                        term.append(pair[0])
-                terms.append(term)
-
         targets = set()
+        for a, b, c, d in permutations(digits, 4):
+            for first_op, second_op, third_op in product(operations, repeat=3):
+                # each expression can be parenthesized in 5 different ways:
+                # ((ab)c)d, (ab)(cd), (a(bc))d, a((bc)d), a(b(cd))
 
-        # bracketify..
-        for term in terms:
-            first_op = term[1]
-            second_op = term[3]
-            third_op = term[5]
+                targets.add(third_op(second_op(first_op(a, b), c), d))
+                targets.add(second_op(first_op(a, b), third_op(c, d)))
+                targets.add(third_op(first_op(a, second_op(b, c)), d))
 
-            # try some possible orders of operations
-            # Note: these are not all possible orders (there're 3! == 6 of them)
-            for tmp in (
-                third_op(second_op(first_op(term[0], term[2]), term[4]), term[6]),
-                second_op(first_op(term[0], term[2]), third_op(term[4], term[6])),
-                third_op(first_op(term[0], second_op(term[2], term[4])), term[6])):
+                # these two can raise ZeroDivisionError
+                try:
+                    targets.add(first_op(a, third_op(second_op(b, c), d)))
+                except ZeroDivisionError:
+                    pass
 
-                if tmp > 0 and float(tmp).is_integer():
-                    targets.add(int(tmp))
+                try:
+                    targets.add(first_op(a, second_op(b, third_op(c, d))))
+                except ZeroDivisionError:
+                    pass
 
-        cons = calc_consecutive(sorted(targets))
+        cons = calc_consecutive(targets)
         if cons > max_consecutive:
             max_consecutive = cons
             answer = digits
     return "".join([str(d) for d in answer])
 
 if __name__ == "__main__":
-    assert calc_consecutive(range(2, 10)) == 0
-    assert calc_consecutive(range(1, 10)) == 9
-    assert calc_consecutive([1, 2, 3, 4, 6, 7, 8, 9]) == 4
+    assert calc_consecutive(set(range(2, 10))) == 0
+    assert calc_consecutive(set(range(1, 10))) == 9
+    assert calc_consecutive(set([1, 2, 3, 4, 6, 7, 8, 9])) == 4
 
     print(main())

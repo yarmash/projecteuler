7dbd006b7d34d7eecc8dc5945ae283b41f401fbd
doer <e.yarmash@gmail.com>
2015-04-03 13:28:42 +0300

    Optimize the solution
    Generate primitive pythagorean triples and check whether they form an
    'almost equilateral triangle'

--- python/problem094.py
+++ python/problem094.py
@@ -2,21 +2,29 @@
 
 """Problem 94: Almost equilateral triangles"""
 
-from utils import isqrt
-
 
 def main():
     max_perimeter = 1000000000
     psum = 0
 
-    for a in range(5, max_perimeter//3+2, 2):
-        tmp = a*a - (a-1)*(a-1)//4
-        if isqrt(tmp)**2 == tmp:
-            psum += a+a+a-1
+    triplets = [(3, 4, 5)]
 
-        tmp = a*a - (a+1)*(a+1)//4
-        if isqrt(tmp)**2 == tmp:
-            psum += a+a+a+1
+    while triplets:
+        a, b, c = triplets.pop()
+
+        if 2*(c + a) > max_perimeter or 2*(c + b) > max_perimeter:
+            continue
+
+        if c - 2*a == 1 or c - 2*a == -1:
+            psum += 2*(c + a)
+
+        if c - 2*b == 1 or c - 2*b == -1:
+            psum += 2*(c + b)
+
+        triplets.extend([
+            (a - 2*b + 2*c, 2*a - b + 2*c, 2*a - 2*b + 3*c),
+            (a + 2*b + 2*c, 2*a + b + 2*c, 2*a + 2*b + 3*c),
+            (-a + 2*b + 2*c, -2*a + b + 2*c, -2*a + 2*b + 3*c)])
 
     return psum
 

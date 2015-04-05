4403485f3bd8939eaacd3ed2309923bcf2378ce1
doer <e.yarmash@gmail.com>
2015-04-05 23:32:40 +0300

    Use a more general/reusable function

--- python/problem001.py
+++ python/problem001.py
@@ -2,14 +2,17 @@
 
 """Problem 1: Multiples of 3 and 5"""
 
-def sum_divisible_by(d, lim=999):
-    """Return the sum of the numbers <= lim that are divisible by d"""
-    n = lim // d
-    return (d + n*d)*n >> 1
+
+def arithmetic_series(first, last, terms):
+    """Return the sum of the members of a finite arithmetic progression"""
+    return terms * (first + last) // 2
+
 
 def main():
-    return sum_divisible_by(3) + sum_divisible_by(5) - sum_divisible_by(15)
-
+    lim = 999
+    return (arithmetic_series(3, lim-lim%3, lim//3) +
+            arithmetic_series(5, lim-lim%5, lim//5) -
+            arithmetic_series(15, lim-lim%15, lim//15))
 
 if __name__ == "__main__":
     print(main())

9e2404037842c59488e8ccdf38549528acf308cf
doer <e.yarmash@gmail.com>
2015-04-14 15:33:23 +0300

    Use a list comprehension

--- python/problem016.py
+++ python/problem016.py
@@ -2,8 +2,9 @@
 
 """Problem 16: Power digit sum"""
 
+
 def main():
-    return sum(int(digit) for digit in str(2**1000))
+    return sum([int(digit) for digit in str(2**1000)])
 
 if __name__ == "__main__":
     print(main())

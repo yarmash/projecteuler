e83ca3dc91b801dd5c06661bc88f388b2fd69c8b
doer <e.yarmash@gmail.com>
2015-04-14 16:08:06 +0300

    Optimize the solution
    Use modular exponentiation

--- python/problem048.py
+++ python/problem048.py
@@ -3,7 +3,7 @@
 """Problem 48: Self powers"""
 
 def main():
-    return sum(i**i for i in range(1, 1001)) % 10**10
+    return sum([pow(i, i, 10**10) for i in range(1, 1001)]) % 10**10
 
 if __name__ == "__main__":
     print(main())

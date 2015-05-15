0b74c79462f4d0a785512aac7b0844a8014f1953
doer <e.yarmash@gmail.com>
2015-05-15 20:10:42 +0300

    Pre-calculate squares
    This is slightly faster and doesn't rely on floating point computation

--- python/problem064.py
+++ python/problem064.py
@@ -2,13 +2,14 @@
 
 """Problem 64: Odd period square roots"""
 
-from math import sqrt
 from utils import sqrt_fraction_expansion as sfe
 
 
 def main():
     lim = 10000
-    return sum(1 for n in range(2, lim+1) if not (sqrt(n).is_integer() or len(sfe(n)) & 1))
+    squares = {x*x for x in range(2, int(lim**.5) + 1)}
+
+    return sum([1 for n in range(2, lim+1) if not (n in squares or len(sfe(n)) & 1)])
 
 if __name__ == "__main__":
     print(main())
--- python/problem066.py
+++ python/problem066.py
@@ -6,8 +6,6 @@
 # http://en.wikipedia.org/wiki/Pell%27s_equation#Fundamental_solution_via_continued_fractions
 # http://en.wikipedia.org/wiki/Continued_fraction#Pell.27s_equation
 
-
-from math import sqrt
 from utils import sqrt_fraction_expansion, convergent_fractions
 
 
@@ -20,10 +18,12 @@ def quotients(n):
             yield i
 
 def main():
+    lim = 1000
     res = max_x = 0
+    squares = {x*x for x in range(2, int(lim**.5)+1)}
 
-    for d in range(2, 1001):
-        if sqrt(d).is_integer():
+    for d in range(2, lim+1):
+        if d in squares:
             continue
 
         for num, den in convergent_fractions(quotients(d)):

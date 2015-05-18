51ca53fa10a38c961ac0e4ea9c3ab25b837ef91a
doer <e.yarmash@gmail.com>
2015-05-19 00:38:02 +0300

    Optimize the solution
    Use the fact that n and n+1 in n*(n+1)/2 for a triangular number are
    necessarily co-prime.

--- python/problem012.py
+++ python/problem012.py
@@ -2,31 +2,37 @@
 
 """Problem 12: Highly divisible triangular number"""
 
-from utils import prime_sieve, triangular_numbers
+from utils import memoize, prime_sieve
+from itertools import count
+
+
+@memoize
+def num_of_divisors(num, primes=prime_sieve(1000)):
+    """Returns the number of divisors of an integer"""
+    cnt = 1
+
+    for p in primes:
+        if p*p > num:
+            cnt *= 2
+            break
+        exponent = 1
+        while num % p == 0:
+            exponent += 1
+            num //= p
+        cnt *= exponent
+
+        if num == 1:
+            break
+    return cnt
 
 
 def main():
-    primes = prime_sieve(65500)
+    for n in count(1):
+        divisors = (num_of_divisors((n+1)//2) * num_of_divisors(n) if n & 1
+                    else num_of_divisors(n//2) * num_of_divisors(n+1))
 
-    for t in triangular_numbers():
-        cnt = 1
-        tt = t
-
-        for p in primes:
-            if p*p > tt:
-                cnt *= 2
-                break
-            exponent = 1
-            while tt % p == 0:
-                exponent += 1
-                tt //= p
-            cnt *= exponent
-
-            if tt == 1:
-                break
-
-        if cnt >= 500:
-            return t
+        if divisors >= 500:
+            return n*(n + 1) >> 1
 
 if __name__ == "__main__":
     print(main())

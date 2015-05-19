2289fa5f071554b97089dc1ae0c3f2b4dfad0077
doer <e.yarmash@gmail.com>
2015-05-19 23:41:17 +0300

    Optimize the solution
    Improve file parsing, cache values for chars

--- python/problem022.py
+++ python/problem022.py
@@ -7,14 +7,15 @@ from utils import open_data_file
 
 def main():
     with open_data_file("names.txt") as data_file:
-        names = data_file.read().split(",")
+        names = data_file.read().split('","')
+    names[0] = names[0][1:]
+    names[-1] = names[-1][:-1]
     names.sort()
-    s = 0
 
-    for pos, name in enumerate(names, 1):
-        s += sum([ord(c) - 64 for c in name if c != '"']) * pos
+    values = {c: i for i, c in enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 1)}
 
-    return s
+    return sum([sum([values[c] for c in name]) * pos
+                for pos, name in enumerate(names, 1)])
 
 if __name__ == "__main__":
     print(main())

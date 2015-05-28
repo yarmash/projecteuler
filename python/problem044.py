14bf379be6ea5aa277ca771d6e92c1f266637dc5
doer <e.yarmash@gmail.com>
2015-05-28 19:59:41 +0300

    Optimize the solution

--- python/problem044.py
+++ python/problem044.py
@@ -2,36 +2,17 @@
 
 """Problem 44: Pentagon numbers"""
 
-from utils import is_pentagonal
+from itertools import count
+from utils import is_pentagonal, nth_pentagonal as P
 
 
 def main():
-
-    pentagonals = []
-    pset = set()
-    min_diff = float("inf")
-    p = diff = 1
-
-    while True:
-        diff += 3  # the difference increases by 3
-        p += diff
-
-        if not p & 1:  # both numbers must be even
-            for i, v in enumerate(reversed(pentagonals)):
-                d = p - v
-                if d > min_diff:
-                    break
-
-                if d in pset and is_pentagonal(p + v):
-                    if d < min_diff:
-                        min_diff = d
-            pentagonals.append(p)
-            pset.add(p)
-
-        if diff > min_diff:
-            break
-
-    return min_diff
+    # P(n+k)-P(n) = 3kn + P(k), P(n+k)-P(n) = P(m)
+    for m in count(1):
+        for k in range(1, m):
+            n, mod = divmod(P(m) - P(k), 3*k)
+            if not mod and is_pentagonal(P(n+k) + P(n)):
+                return P(m)
 
 if __name__ == "__main__":
     print(main())

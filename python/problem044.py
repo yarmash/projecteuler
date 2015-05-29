970202c92bb09fdc9ef8f69ee57b1f672618b876
doer <e.yarmash@gmail.com>
2015-05-29 15:51:22 +0300

    Optimize the solution
    Calculate P(x) dynamically, skip odd numbers.

--- python/problem044.py
+++ python/problem044.py
@@ -3,17 +3,22 @@
 """Problem 44: Pentagon numbers"""
 
 from itertools import count
-from utils import is_pentagonal, nth_pentagonal as P
+from utils import is_pentagonal
 
 
 def main():
     # P(n+k)-P(n) = 3kn + P(k), P(n+k)-P(n) = P(m)
+    pm = 1
     for m in count(1):
-        pm = P(m)
-        for k in range(1, m):
-            n, mod = divmod(pm - P(k), 3*k)
-            if not mod and is_pentagonal(pm + 2*P(n)):
-                return pm
+        if not pm & 1:  # both numbers must be even
+            pk = 1
+            for k in range(1, m):
+                if not (pm - pk) % (3*k):
+                    n = (pm - pk) // (3*k)
+                    if is_pentagonal(pm + n*(3*n - 1)):
+                        return pm
+                pk += 3*k + 1
+        pm += 3*m + 1
 
 if __name__ == "__main__":
     print(main())

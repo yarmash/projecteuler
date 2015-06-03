b30ca36b26e481e46c67f667bb07ba400c6f23d9
doer <e.yarmash@gmail.com>
2015-06-03 20:11:19 +0300

    Get rid of the 'Evaluator' class, use more dunder methods

--- python/problem054.py
+++ python/problem054.py
@@ -3,28 +3,30 @@
 """Problem 54: Poker hands"""
 
 from utils import open_data_file
-from operator import attrgetter
 
 
 class Card(object):
-    values = dict(zip("23456789TJQKA", range(2, 15)))
-    suits = {s[0]: s for s in ("Clubs", "Diamonds", "Hearts", "Spades")}
+    values = {kind: val for val, kind in enumerate("23456789TJQKA", 2)}
     __slots__ = ("kind", "value", "suit")
 
-    def __init__(self, data):
-        self.kind, self.suit = data  # example: 9C JD
-        self.value = Card.values[self.kind]
+    def __init__(self, kind, suit):
+        self.kind = kind
+        self.suit = suit
+        self.value = Card.values[kind]
 
     def __repr__(self):
-        return "%s%s" % (self.kind, Card.suits[self.suit])
+        return "".join([self.kind, self.suit])
+
+    def __lt__(self, other):
+        """Make instances of the class sortable"""
+        return self.value < other.value
 
 
 class Hand(object):
     __slots__ = ("cards", "same_suit", "consecutive_values")
 
     def __init__(self, cards):
-        cards.sort(key=attrgetter("value"), reverse=True)
-
+        cards.sort(reverse=True)
         self.cards = cards
 
         self.same_suit = cards[0].suit == cards[1].suit == cards[2].suit == \
@@ -33,13 +35,17 @@ class Hand(object):
         self.consecutive_values = cards[0].value == cards[1].value + 1 == \
             cards[2].value + 2 == cards[3].value + 3 == cards[4].value + 4
 
+    def __lt__(self, other):
+        """Make instances of the class comparable"""
+        for ours, theirs in zip(self.terms(), other.terms()):
+            if ours == theirs:
+                continue
+            return ours < theirs
 
-class Evaluator(object):
-    @staticmethod
-    def terms(hand):
-        """Generate terms for hands' comparison"""
+    def terms(self):
+        """Generate terms for comparison"""
 
-        cards = hand.cards
+        cards = self.cards
 
         # save the number of occurrences for each value
         occurrences = [0]*15
@@ -50,7 +56,7 @@ class Evaluator(object):
         # Is a case of the straight flush.
 
         # Straight Flush: All cards are consecutive values of same suit.
-        if hand.consecutive_values and hand.same_suit:
+        if self.consecutive_values and self.same_suit:
             yield cards[0].value
         else:
             yield 0
@@ -68,13 +74,13 @@ class Evaluator(object):
             yield 0
 
         # Flush: All cards of the same suit.
-        if hand.same_suit:
+        if self.same_suit:
             yield cards[0].value
         else:
             yield 0
 
         # Straight: All cards are consecutive values.
-        if hand.consecutive_values:
+        if self.consecutive_values:
             yield cards[0].value
         else:
             yield 0
@@ -111,23 +117,15 @@ class Evaluator(object):
         # High Card: Highest value card.
         yield cards[0].value
 
-    def compare(self, hand1, hand2):
-        for term1, term2 in zip(self.terms(hand1), self.terms(hand2)):
-            if term1 == term2:
-                continue
-            if term1 > term2:
-                return 1
-            return -1
-
 
 def main():
     cnt = 0
-    evaluator = Evaluator()
 
     with open_data_file("poker.txt") as data_file:
         for line in data_file:
-            cards = [Card(c) for c in line.split()]
-            if evaluator.compare(Hand(cards[:5]), Hand(cards[5:])) > 0:
+            cards = [Card(*card) for card in line.split()]  # example: 9C JD
+
+            if Hand(cards[:5]) > Hand(cards[5:]):
                 cnt += 1
 
     return cnt

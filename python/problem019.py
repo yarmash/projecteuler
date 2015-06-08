30474d3194875a851ec661d387c08b487e75a1c4
doer <e.yarmash@gmail.com>
2015-06-08 19:29:04 +0300

    Short circuit at the first test

--- python/problem019.py
+++ python/problem019.py
@@ -2,8 +2,11 @@
 
 """Problem 19: Counting Sundays"""
 
-def is_leap_year(y):
-    return y % 4 == 0 and y % 100 != 0 or y % 400 == 0
+
+def is_leap_year(year):
+    """Determine whether a year is a leap year"""
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
 
 def main():
     numdays = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)

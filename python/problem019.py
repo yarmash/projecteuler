311a56b9750a16570e205ed68fa036e058e6c674
doer <e.yarmash@gmail.com>
2015-06-09 12:14:47 +0300

    Check for a leap year first, then for month

--- python/problem019.py
+++ python/problem019.py
@@ -11,14 +11,15 @@ def is_leap_year(year):
 def main():
     numdays = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
-    year, month, wday = 1901, 0, 2  # 1 Jan 1901
+    year, month, wday = 1901, 0, 2  # Tue, 1 Jan 1901
+    is_leap = False
     sundays = 0
 
     while year < 2001:
         if wday == 0:
             sundays += 1
 
-        days = 29 if month == 1 and is_leap_year(year) else numdays[month]
+        days = 29 if is_leap and month == 1 else numdays[month]
         wday = (wday + days) % 7
 
         if month < 11:
@@ -26,6 +27,7 @@ def main():
         else:
             month = 0
             year += 1
+            is_leap = is_leap_year(year)
 
     return sundays
 

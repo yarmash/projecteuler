10cb19518c7f3466dc55e94225f97270bfcd32aa
doer <e.yarmash@gmail.com>
2015-06-29 11:53:51 +0300

    Optimize the solution
    Use "".join to get the number

--- python/problem041.py
+++ python/problem041.py
@@ -7,13 +7,13 @@ from utils import is_prime, is_pandigital
 
 
 def main():
-    digits = (7, 6, 5, 4, 3, 2, 1)
+    digits = "987654321"
 
-    for ndigits in [7, 4]: # only 7 or 4 digit numbers need to be considered (in other cases the sum is divisible by 3)
-        for p in permutations(digits[7-ndigits:], ndigits):
-            n = 0
-            for i in range(ndigits):
-                n += 10**(ndigits-i-1)*p[i]
+    # only 7- or 4-digit numbers need to be considered
+    # in other cases the sum is divisible by 3
+    for ndigits in (7, 4):
+        for p in permutations(digits[-ndigits:], ndigits):
+            n = int("".join(p))
 
             if is_pandigital(n, ndigits) and is_prime(n):
                 return n

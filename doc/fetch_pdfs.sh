eec079276242da762a53a90142c688a17bd0f5f1
doer <e.yarmash@gmail.com>
2015-07-02 01:25:02 +0300

    Add an overview for the problem 15

---
+++ doc/fetch_pdfs.sh
@@ -0,0 +1,9 @@
+rm *.pdf
+
+for problem in $(seq -w 1 $(cat ../python/answers  | wc -l)); do
+	curl -v -L -O -J --max-redirs 0 \
+		--user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0" \
+		--cookie "PHPSESSID=c311032ee5b4c159b261d5395965f285" \
+		https://projecteuler.net/overview=$problem
+	sleep $((RANDOM % 10 + 1))
+done

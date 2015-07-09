40eb6a47295d6f864d4f992d2c01008d1d5e2a45
doer <e.yarmash@gmail.com>
2015-07-10 00:21:37 +0300

    Optimize the solution
    Use some combinatorics magic

--- python/problem106.py
+++ python/problem106.py
@@ -2,19 +2,23 @@
 
 """Problem 106: Special subset sums: meta-testing"""
 
-from itertools import combinations
+from utils import n_choose_k
+
+
+def nth_catalan_number(n):
+    """
+    Return the nth Catalan number
+    https://en.wikipedia.org/wiki/Catalan_number
+    """
+    return n_choose_k(2*n, n) // (n + 1)
 
 
 def main():
-    s = set(range(1, 13))
-    pairs = 0
-
-    for i in range(2, len(s)//2+1):
-        for a in combinations(s, i):
-            for b in combinations(s - set(a), i):
-                if a < b and any(x > y for x, y in zip(a, b)):
-                    pairs += 1
-    return pairs
+    n = 12
+    return sum(
+        # the catalan numbers specify subsets that don't need to be tested
+        [n_choose_k(n, 2*i) * (n_choose_k(2*i, i)//2 - nth_catalan_number(i))
+         for i in range(2, n//2+1)])
 
 if __name__ == "__main__":
     print(main())

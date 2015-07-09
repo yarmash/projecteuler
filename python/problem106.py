d20a9ad6ba3e1c6fd0584f5e1a67171118174a49
doer <e.yarmash@gmail.com>
2015-07-09 22:30:35 +0300

    Optimize the solution
    Skip non-disjoint sets and sets with unequal size

--- python/problem106.py
+++ python/problem106.py
@@ -2,26 +2,19 @@
 
 """Problem 106: Special subset sums: meta-testing"""
 
-from itertools import chain, combinations
+from itertools import combinations
 
 
 def main():
-    s = list(range(1, 13))
+    s = set(range(1, 13))
+    pairs = 0
 
-    cnt = 0
-
-    for pair in combinations(chain.from_iterable(
-            [combinations(s, r) for r in range(2, len(s)//2+1)]), 2):
-
-        if len(pair[0]) != len(pair[1]):
-            continue
-        if set(pair[0]) & set(pair[1]):
-            continue
-        if all(pair[0][i] < pair[1][i] for i in range(len(pair[0]))):
-            continue
-
-        cnt += 1
-    return cnt
+    for i in range(2, len(s)//2+1):
+        for a in combinations(s, i):
+            for b in combinations(s - set(a), i):
+                if a < b and any(x > y for x, y in zip(a, b)):
+                    pairs += 1
+    return pairs
 
 if __name__ == "__main__":
     print(main())

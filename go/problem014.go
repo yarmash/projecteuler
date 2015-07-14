dfa2e106f0de5347a0a7cedc11c9e05e5868bc0c
doer <e.yarmash@gmail.com>
2015-07-15 02:54:18 +0300

    Optimize the solution

--- go/problem014.go
+++ go/problem014.go
@@ -1,32 +1,34 @@
 package main
 
-import "fmt"
-
-var cache = map[int]int{2: 2}
-
-func get_chain_len(n int) int {
-
-	if cache[n] == 0 {
-		if n%2 == 0 {
-			cache[n] = 1 + get_chain_len(n/2)
-		} else {
-			cache[n] = 2 + get_chain_len((3*n+1)/2)
-		}
-	}
-	return cache[n]
-}
-
 func main() {
 	const lim = 1000000
+	cache := make([]int, lim)
+	cache[2] = 2
+
 	longest := 0
 	num := 0
 
-	for i := lim / 2; i < lim; i++ {
-		length := get_chain_len(i)
-		if length > longest {
-			longest = length
-			num = i
+	for n := 3; n < lim; n++ {
+
+		chain_len := 0
+		var next = n
+
+		for next >= n {
+			if next%2 == 1 {
+				next = (next*3 + 1) / 2
+				chain_len += 2
+			} else {
+				next /= 2
+				chain_len += 1
+			}
+		}
+		chain_len += cache[next]
+		cache[n] = chain_len
+
+		if chain_len > longest {
+			longest = chain_len
+			num = n
 		}
 	}
-	fmt.Printf("%d\n", num)
+	println(num)
 }

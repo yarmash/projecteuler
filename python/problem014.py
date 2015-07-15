dc9e2b5aa8629037d8512fb4a830c8b40a4e61ec
doer <e.yarmash@gmail.com>
2015-07-15 18:29:12 +0300

    Optimize the solution
    Use a list of 10**6 elements as a cache.

--- python/problem014.py
+++ python/problem014.py
@@ -2,20 +2,34 @@
 
 """Problem 14: Longest Collatz sequence"""
 
+
 def main():
 
-    def get_chain_length(n, cache={2: 2}):
-        if not n in cache:
-            cache[n] = 2 + get_chain_length((3*n+1)>>1) if n&1 else 1 + get_chain_length(n>>1)
-        return cache[n]
+    lim = 1000000
 
-    limit = 1000000
+    cache = [False]*lim
+    cache[2] = 2
+
+    def get_chain_len(n, cache=cache):
+        """
+        Return chain length for a number, cache final and intermediate results.
+        """
+        if n < lim:
+            if cache[n]:
+                return cache[n]
+            chain_len = cache[n] = (2 + get_chain_len((3*n + 1) >> 1) if n & 1
+                                    else 1 + get_chain_len(n >> 1))
+        else:
+            chain_len = (2 + get_chain_len((3*n + 1) >> 1) if n & 1
+                         else 1 + get_chain_len(n >> 1))
+        return chain_len
+
     longest = num = 0
 
     # Look only at the second half of the numbers in the range
-    # For every number n ≤ limit//2, 2n is also in the range
-    for i in range(limit//2, limit):
-        length = get_chain_length(i)
+    # Every number n ≤ lim//2 has a reverse map n*2**k in the upper half.
+    for i in range(lim//2, lim):
+        length = get_chain_len(i)
 
         if length > longest:
             longest = length

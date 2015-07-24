34737342b27bc67cce28baeb93e35ef29939f5af
doer <e.yarmash@gmail.com>
2015-07-24 19:42:23 +0300

    Optimize the solution
    Use permutations, avoid int-to-string conversions

--- python/problem032.py
+++ python/problem032.py
@@ -3,20 +3,34 @@
 """Problem 32: Pandigital products"""
 
 from utils import is_pandigital
+from itertools import permutations
+
 
 def main():
     products = set()
+    digits = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-    # the solutions must either be 1x4 or 2x3 digits
-    for r in (((2, 10), (1234, 9877)), ((12, 82), (123, 988))):
-        for a in range(*r[0]):
-            if not a % 5: continue
-            for b in range(*r[1]):
-                if not b % 5: continue
-                p = a*b
-                if p > 9876: break
-                if is_pandigital(int(str(a)+str(b)+str(p))):
-                    products.add(p)
+    # The factors must either be 1, 4 or 2, 3 digits.
+
+    # skip 1 (1 x N == N)
+    for a in digits[1:]:
+        for b in permutations([d for d in digits if d != a], 4):
+            b = b[0]*1000 + b[1]*100 + b[2]*10 + b[3]
+            prod = a*b
+            if prod > 9876:
+                break
+            if is_pandigital(a*10**8 + b*10**4 + prod):
+                products.add(prod)
+
+    for perm in permutations(digits, 2):
+        a = perm[0]*10 + perm[1]
+        for b in permutations([d for d in digits if d not in perm], 3):
+            b = b[0]*100 + b[1]*10 + b[2]
+            prod = a*b
+            if prod > 9876:
+                break
+            if is_pandigital(a*10**7 + b*10**4 + prod):
+                products.add(prod)
 
     return sum(products)
 

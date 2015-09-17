1106846f9327a9872f420bc4a55769b133e73c8e
doer <e.yarmash@gmail.com>
2015-09-17 19:57:34 +0300

    Use isqrt() for computing integer square root.
    From https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division:
    
    floor(sqrt(n)) is not necessarily a fixed point of the iterative formula.
    If n + 1 is a perfect square, the sequence ends up in a period-two cycle
    between floor(sqrt(n)) and floor(sqrt(n)) + 1 instead of converging.
    
    Also includes some minor optimizations.

--- python/problem080.py
+++ python/problem080.py
@@ -2,27 +2,18 @@
 
 """Problem 80: Square root digital expansion"""
 
-from math import sqrt
+from utils import isqrt
+
 
 def main():
-    # http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
 
-    squares = {x*x for x in range(1, 11)}
-    res = 0
+    total = 0
 
-    for num in (x for x in range(1, 101) if not x in squares):
-        num *= 10**200
-        root = int(sqrt(num))
-
-        while True:
-            new = (root + num//root) >> 1
-            if new == root:
-                break
-            root = new
-
-        res += sum(int(x) for x in ("%i" % root)[:100])
-    return res
-
+    for num in range(2, 100):
+        if num not in {4, 9, 16, 25, 36, 49, 64, 81}:
+            root = isqrt(num * (10**2)**99)
+            total += sum([int(x) for x in str(root)])
+    return total
 
 if __name__ == "__main__":
     print(main())

c34404ecade8e51427d69067d0b0e433d7cd5ed4
doer <e.yarmash@gmail.com>
2016-01-11 03:04:33 +0300

    Fix few pylint warnings + minor improvements

--- python/add_docstrings.py
+++ python/add_docstrings.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python
 
+"""Add docstrings to Python modules"""
+
 import os
 from itertools import count
 import requests
 from bs4 import BeautifulSoup
 
-for num in count(1):
-    filename = "problem{:03d}.py".format(num)
 
-    if not os.path.exists(filename):
-        break
+def main():
+    for num in count(1):
+        filename = "problem{:03d}.py".format(num)
 
-    url = "https://projecteuler.net/problem="+str(num)
-    r = requests.get(url)
-    r.raise_for_status()
-    s = BeautifulSoup(r.content)
-    problem_number = s.body.find('h3').text
-    problem_title = s.body.find('h2').text
+        if not os.path.exists(filename):
+            break
 
-    docstring = '"""{}: {}"""\n'.format(problem_number, problem_title)
-    print(docstring[3:-4])
+        url = "https://projecteuler.net/problem="+str(num)
+        r = requests.get(url)
+        r.raise_for_status()
+        soup = BeautifulSoup(r.text, "html.parser")
+        problem_number = soup.body.find("h3").text
+        problem_title = soup.body.find("h2").text
 
-    with open(filename, "r+") as f:
-        lines = f.readlines()
+        docstring = '"""{}: {}"""\n'.format(problem_number, problem_title)
+        print(docstring[3:-4])
 
-        if lines[2] != docstring:
-            lines[2:2] = [docstring, "\n"]
-            f.seek(0)
-            f.writelines(lines)
+        with open(filename, "r+") as f:
+            lines = f.readlines()
+
+            if lines[2] != docstring:
+                lines[2:2] = [docstring, "\n"]
+                f.seek(0)
+                f.writelines(lines)
+
+if __name__ == "__main__":
+    main()

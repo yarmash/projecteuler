2a8373a59f3f8762f6cb07083bf1eede857160d1
doer <e.yarmash@gmail.com>
2016-07-26 00:00:31 +0300

    Reuse problem018.main()

--- python/problem067.py
+++ python/problem067.py
@@ -4,15 +4,15 @@
 
 from utils import open_data_file
 
-# This is a more difficult version of Problem 18 and uses the same function.
-from problem018 import calc_max_total
+# This is a more difficult version of Problem 18.
+import problem018
 
 
 def main():
     with open_data_file("triangle.txt") as data_file:
-        nums = [[int(d) for d in line.split()] for line in data_file]
+        triangle = [[int(d) for d in line.split()] for line in data_file]
 
-    return calc_max_total(nums)
+    return problem018.main(triangle)
 
 if __name__ == "__main__":
     print(main())

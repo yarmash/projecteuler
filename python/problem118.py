2a36709438b4573f51efc3fb3329a8eb7f66fc23
doer <e.yarmash@gmail.com>
2017-04-30 00:05:47 +0300

    Optimize the solution

--- python/problem118.py
+++ python/problem118.py
@@ -2,47 +2,44 @@
 
 """Problem 118: Pandigital prime sets"""
 
-from itertools import permutations, chain, combinations
+from itertools import permutations
 
 from utils import prime_sieve
 
 
-def index_sets(indexes=range(1, 9)):
-    """
-    Find possible places where we can split a string of digits to numbers.
-    """
-    return chain.from_iterable(combinations(indexes, r) for r in range(1, 9))
-
-
 def main():
-    primes = frozenset(prime_sieve(100_000_000))
-    digits = "123456789"
-    prime_sets = set()
+    primes = prime_sieve(100_000)
+    primes_set = frozenset(primes)
 
-    for p in permutations(digits, 9):
-        if p[-1] in {"2", "4", "5", "6", "8"}:
-            continue
+    def is_prime(n, primes=primes, primes_set=primes_set):
+        if n < 100_000:
+            return n in primes_set
 
-        for index_set in index_sets():
-            prev = 0
-            parts = []
+        for p in primes:
+            if not n % p:
+                return False
+            if p*p > n:
+                return True
 
-            for i in chain(index_set, [9]):
-                if i-prev > 1:
-                    if p[i-1] in {"2", "4", "5", "6", "8"}:
-                        break
-                elif p[i-1] in {"1", "4", "6", "8", "9"}:
-                    break
-                x = int("".join(p[prev:i]))
-                if x not in primes:
-                    break
-                parts.append(x)
-                prev = i
-            else:
-                prime_set = frozenset(parts)
-                if prime_set not in prime_sets:
-                    prime_sets.add(prime_set)
-    return len(prime_sets)
+    def count_sets(cnt, prev, digits):
+        if not digits:
+            return cnt + 1
+
+        for i in range(1, len(digits)+1):
+            for p in permutations(digits, i):
+                if i == 1:
+                    if p[0] not in "2357":
+                        continue
+                elif p[-1] not in "1379":
+                    continue
+
+                x = int("".join(p))
+
+                if x > prev and is_prime(x):
+                    cnt = count_sets(cnt, x, digits.difference(p))
+        return cnt
+
+    return count_sets(0, 0, frozenset("123456789"))
 
 
 if __name__ == "__main__":

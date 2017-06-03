ab7bbbc038093f01cd2873893eeceb9f5cbea699
doer <e.yarmash@gmail.com>
2017-06-04 00:00:38 +0300

    Optimize more

--- python/problem120.py
+++ python/problem120.py
@@ -8,7 +8,7 @@ def main():
     # For a ≥ 3, 2 < 2*1*a < a*a, so the maximal remainder is obtained for
     # some odd exponent n. To maximize r, 2*n must be the largest
     # even number less than a.
-    return sum(a*(a-1 if a & 1 else a-2) for a in range(3, 1001))
+    return sum(2*a*a - a - 1 for a in range(3, 1001, 2))
 
 
 if __name__ == "__main__":

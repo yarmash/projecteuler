9f924c875a64d72c68b00a0cb95a5a78150a35c1
doer <e.yarmash@gmail.com>
2017-06-25 18:21:05 +0300

    Use num_of_divisors() from utils

--- python/problem012.py
+++ python/problem012.py
@@ -2,30 +2,14 @@
 
 """Problem 12: Highly divisible triangular number"""
 
-from utils import prime_sieve
 from itertools import count
-from functools import lru_cache
+from functools import lru_cache, partial
+
+from utils import prime_sieve, num_of_divisors
 
 
-@lru_cache(maxsize=None)
-def num_of_divisors(num, primes=prime_sieve(1000)):
-    """Returns the number of divisors of an integer"""
-    cnt = 1
-
-    for p in primes:
-        if p*p > num:
-            cnt *= 2
-            break
-        exponent = 1
-        while num % p == 0:
-            exponent += 1
-            num //= p
-        cnt *= exponent
-
-        if num == 1:
-            break
-    return cnt
-
+num_of_divisors = lru_cache(maxsize=None)(partial(num_of_divisors,
+                                                  primes=prime_sieve(1000)))
 
 def main():
     for n in count(1):
@@ -33,7 +17,8 @@ def main():
                     else num_of_divisors(n//2) * num_of_divisors(n+1))
 
         if divisors >= 500:
-            return n*(n + 1) >> 1
+            return n*(n + 1)//2
+
 
 if __name__ == "__main__":
     print(main())

8991fbed0b80d2639af917ee8f1fa420b9ead724
doer <e.yarmash@gmail.com>
2017-07-29 14:31:00 +0300

    Optimize the solution

--- python/problem121.py
+++ python/problem121.py
@@ -2,24 +2,17 @@
 
 """Problem 121: Disc game prize fund"""
 
-from fractions import Fraction
 from itertools import chain, combinations
 from operator import mul
 from functools import reduce
+from math import factorial
 
 
 def main():
-    TURNS = 23
-
-    probs = [(Fraction(x - 1, x), Fraction(1, x)) for x in range(2, TURNS + 2)]
-    prob = 0
-
-    for c in chain.from_iterable(combinations(range(TURNS), n)
-                                 for n in range(TURNS//2 + 1, TURNS+1)):
-
-        prob += reduce(mul, (probs[i][i in c] for i in range(TURNS)))
-
-    return prob.denominator//prob.numerator
+    turns = 15
+    prob = sum(reduce(mul, reds, 1) for reds in chain.from_iterable(
+        combinations(range(1, turns+1), n) for n in range((turns+1)//2)))
+    return factorial(turns+1) // prob
 
 
 if __name__ == "__main__":

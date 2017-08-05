f5fffbfff8768cce5755381b1fbcddef27c02349
doer <e.yarmash@gmail.com>
2017-08-05 20:21:40 +0300

    Add a solution to the problem 122

---
+++ python/problem122.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""Problem 122: Efficient exponentiation"""
+
+from collections import deque
+
+
+def main():
+    lim = 200
+
+    cache = {}
+    queue = deque([[1]])
+
+    # generate star addition chains
+    while queue:
+        chain = queue.popleft()
+
+        for x in chain:
+            y = chain[-1] + x
+            if y > lim:
+                break
+
+            if cache.get(y, lim) >= len(chain):
+                cache[y] = len(chain)
+                queue.append(chain + [y])
+    return sum(cache.values())
+
+
+if __name__ == "__main__":
+    print(main())

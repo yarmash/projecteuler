b348b2474d02f8cba890fb7c8d835722c1197b30
doer <e.yarmash@gmail.com>
2017-08-06 15:32:43 +0300

    Optimize the solution
    Use a sieve to compute the radicals

--- python/problem124.py
+++ python/problem124.py
@@ -2,26 +2,18 @@
 
 """Problem 124: Ordered radicals"""
 
-from functools import reduce
-from math import sqrt
-
-from utils import prime_sieve, prime_factors
-
 
 def main():
     lim = 100_000
 
-    primes = prime_sieve(int(sqrt(lim)))
+    rads = [1]*(lim+1)
 
-    def rad(n):
-        """
-        Calculates the radical of n, i.e. the product of distinct
-        prime factors of n.
-        """
-        factors = prime_factors(n, primes)
-        return reduce(lambda x, y: x*y[0], factors, 1)
+    for i in range(2, lim+1):
+        if rads[i] == 1:
+            for j in range(i, lim+1, i):
+                rads[j] *= i
 
-    return sorted(range(1, lim+1), key=rad)[9999]
+    return sorted(range(lim+1), key=rads.__getitem__)[10000]
 
 
 if __name__ == "__main__":

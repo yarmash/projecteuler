9a0b53e04be31b82c578463190fc7c067c027d85
doer <e.yarmash@gmail.com>
2017-12-26 02:56:02 +0300

    Optimize the solution
    Sort the radicals

--- python/problem127.py
+++ python/problem127.py
@@ -8,17 +8,28 @@ from math import gcd
 def main():
     lim = 120000
 
-    rads = [1]*(lim+1)
+    rads = [1]*lim
 
-    for i in range(2, lim+1):
+    for i in range(2, lim):
         if rads[i] == 1:
-            for j in range(i, lim+1, i):
+            for j in range(i, lim, i):
                 rads[j] *= i
+
+    numbers = sorted(range(1, lim), key=rads.__getitem__)
+
     s = 0
 
-    for a in range(1, lim):
-        for b in range(a+1, lim-a):
-            c = a + b
+    for c in range(3, lim):
+        for a in numbers:
+            if a >= c // 2:  # ensure b > a
+                continue
+
+            if rads[a]*rads[c] >= c:
+                break
+
+            b = c - a
+
+            # gcd(a, b) == 1 implies gcd(a, c) == gcd(b, c) == 1
             if rads[a]*rads[b]*rads[c] < c and gcd(a, b) == 1:
                 s += c
     return s

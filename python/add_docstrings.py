ddcb2f2e94aa824ac41a152abe53917388ccdcde
doer <e.yarmash@gmail.com>
2018-03-02 10:59:04 +0300

    Use asyncio

--- python/add_docstrings.py
+++ python/add_docstrings.py
@@ -3,12 +3,12 @@
 """Add docstrings to solutions."""
 
 import argparse
+import asyncio
 import sys
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
-import requests
 from bs4 import BeautifulSoup
-
+import aiohttp
+import async_timeout
 from utils import get_path
 
 
@@ -16,21 +16,15 @@ def parse_args(args=None) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Add docstrings to solutions")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-n", "--number", type=int, metavar="NUM",
-                       help="The problem number")
+    group.add_argument("path", nargs="*", default=[],
+                       help="A path (or paths) to the module file")
     group.add_argument("-a", "--all", action="store_true",
                        help="Process all files")
 
     return parser.parse_args(args)
 
 
-def fetch_url(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
-
-
-def get_docstring(html):
+def get_docstring(html) -> str:
     soup = BeautifulSoup(html, "html.parser")
     problem_number = soup.body.find("h3").contents[0].strip()
     problem_title = soup.body.find("h2").text
@@ -48,31 +42,46 @@ def update_file(path, docstring):
             f.writelines(lines)
 
 
-def main():
-    args = parse_args()
+async def fetch_url(session, url):
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            return url, await response.text()
 
-    if args.number:
-        path = get_path("python", f"problem{args.number:03d}.py")
-        if not path.exists():
-            print(f"Module not found: {path}")
-            return 1
-        paths = [path]
-    else:
-        paths = sorted(get_path("python").glob("problem???.py"))
 
-    with ThreadPoolExecutor() as executor:
-        futures = {}
-
+async def add_docstrings(paths):
+    async with aiohttp.ClientSession() as session:
+        coros = []
+        url2path = {}
         for path in paths:
             num = path.stem[-3:]  # extract the problem number
             url = f"https://projecteuler.net/problem={num}"
-            futures[executor.submit(fetch_url, url)] = path
+            url2path[url] = path
+            coros.append(fetch_url(session, url))
 
-        for future in as_completed(futures):
-            html = future.result()
-            path = futures[future]
+        for future in asyncio.as_completed(coros):
+            url, html = await future
+            path = url2path[url]
             docstring = get_docstring(html)
             update_file(path, docstring)
+
+
+def main():
+    args = parse_args()
+
+    if args.path:
+        paths = []
+        for path in args.path:
+            path_obj = get_path("python", path)
+            if not path_obj.exists():
+                print(f"File not found: {path}")
+                return 1
+            paths.append(path_obj)
+    else:
+        paths = sorted(get_path("python").glob("problem???.py"))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(add_docstrings(paths))
+    return 0
 
 
 if __name__ == "__main__":

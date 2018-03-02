c1685050e625e0b7a183f9e484613529daf389cf
doer <e.yarmash@gmail.com>
2018-03-02 16:32:52 +0300

    Use math.hypot as an optimization

--- python/problem086.py
+++ python/problem086.py
@@ -3,14 +3,14 @@
 """Problem 86: Cuboid route"""
 
 from itertools import count
-from math import sqrt
+from math import hypot
 
 
 def main():
     cnt = 0
     for a in count(1):
         for s in range(2, 2*a+1):
-            if sqrt(a*a + s*s).is_integer():
+            if hypot(a, s).is_integer():
                 cnt += min(s, a+1) - (s+1)//2
 
         if cnt > 1000000:

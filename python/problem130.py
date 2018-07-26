0309da92ee660d7b34ed6352bdf7bad8bae17d06
doer <e.yarmash@gmail.com>
2018-07-26 17:25:45 +0300

    Optimize the solution

--- python/problem130.py
+++ python/problem130.py
@@ -3,7 +3,6 @@
 """Problem 130: Composites with prime repunit property"""
 
 from itertools import count
-from math import log10, ceil
 
 from utils import is_prime
 
@@ -12,18 +11,11 @@ def main():
     composites = []
 
     for n in count(9, 2):
-        if n % 5 and not is_prime(n):
-            k = ceil(log10(n*3))
-            r = (10**k-1)//9
-
-            while r:
-                r = (r*10 + 1) % n
-                k += 1
-
-            if not (n - 1) % k:
-                composites.append(n)
-                if len(composites) == 25:
-                    return sum(composites)
+        # A(n) divides n-1 iff 10^(n-1)=1 (mod 9*n)
+        if n % 5 and not is_prime(n) and pow(10, n - 1, 9*n) == 1:
+            composites.append(n)
+            if len(composites) == 25:
+                return sum(composites)
 
 
 if __name__ == "__main__":

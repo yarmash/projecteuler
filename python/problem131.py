e7fde8540831e1505d09767f52461617db4fd48f
doer <e.yarmash@gmail.com>
2018-07-30 15:25:42 +0300

    Add a solution to the problem 131

---
+++ python/problem131.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from itertools import count
+
+from utils import is_prime
+
+
+def main():
+    lim = 1_000_000
+    res = 0
+
+    for x in count(1):
+        n = (x+1)**3 - x**3
+        if n > lim:
+            break
+        if is_prime(n):
+            res += 1
+    return res
+
+
+if __name__ == "__main__":
+    print(main())

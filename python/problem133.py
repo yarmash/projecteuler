01149d376716d7a83e4bf0cf5decfff6c9c6e443
doer <e.yarmash@gmail.com>
2018-07-30 12:13:14 +0300

    Optimize the solution

--- python/problem133.py
+++ python/problem133.py
@@ -6,32 +6,26 @@
 from utils import prime_sieve
 
 
-def A(n):
-    r = k = 1
-
-    while r:
-        r = (r*10 + 1) % n
-        k += 1
-    return k
-
-
-def check(n):
-    while n % 2 == 0:
-        n //= 2
-    while n % 5 == 0:
-        n //= 5
-    return n == 1
-
-
 def main():
     primes = prime_sieve(100_000)
-    factors = []
+    res = 0
 
     for prime in primes:
-        if prime > 5 and check(A(prime)):
-            factors.append(prime)
+        if prime < 7:
+            res += prime
+            continue
+        f1, f2 = prime - 1, 1
+        while f1 % 2 == 0:
+            f1 //= 2
+            f2 *= 2
+        while f1 % 5 == 0:
+            f1 //= 5
+            f2 *= 5
+        if f1 == 1 or pow(10, f2, prime) == 1:
+            continue
+        res += prime
 
-    return sum(primes) - sum(factors)
+    return res
 
 
 if __name__ == "__main__":

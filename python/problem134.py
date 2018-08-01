2f34bd8d2e47d0b2512e42b81dd19c6af4985cfa
doer <e.yarmash@gmail.com>
2018-08-01 14:19:38 +0300

    Add a (brute-force) solution to the problem 134

---
+++ python/problem134.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""Problem 134: Prime pair connection"""
+
+from itertools import tee, count
+from math import log10, ceil
+
+from utils import prime_sieve_lazy
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def main():
+    lim = 1_000_000
+    primes = prime_sieve_lazy()
+    next(primes)
+    next(primes)
+    res = 0
+
+    # p1 + d * k == 0 (mod p2).
+    for p1, p2 in pairwise(primes):
+        if p1 > lim:
+            break
+
+        d = 10**(ceil(log10(p1)))
+
+        for k in count():
+            if (p1 + d*k) % p2 == 0:
+                res += p1 + d*k
+                break
+    return res
+
+
+if __name__ == "__main__":
+    print(main())

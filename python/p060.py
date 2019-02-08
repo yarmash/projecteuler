08d8f44f465f7031576958309ced1b7422feb78d
doer <e.yarmash@gmail.com>
2019-02-08 21:18:10 +0300

    Use f-strings and unpacking

--- python/p060.py
+++ python/p060.py
@@ -12,19 +12,19 @@ def main():
 
     @lru_cache(maxsize=None)
     def check(p1, p2):
-        return is_prime(int(str(p1)+str(p2))) and is_prime(int(str(p2)+str(p1)))
+        return is_prime(int(f'{p1}{p2}')) and is_prime(int(f'{p2}{p1}'))
 
     def candidates(p):
         for i in range(p[-1]+1, len(primes)):
             if all(check(primes[i], primes[j]) for j in p):
                 if len(p) == 4:
-                    yield p+[i]
+                    yield (*p, i)
                 else:
-                    yield from candidates(p+[i])
+                    yield from candidates((*p, i))
 
     for i in range(len(primes)):
         try:
-            quint = next(candidates([i]))
+            quint = next(candidates((i,)))
             return sum(primes[x] for x in quint)
         except StopIteration:
             pass

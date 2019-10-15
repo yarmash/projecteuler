200b4ffcd61aa86e263db165f0af7d8edb7eae06
doer <e.yarmash@gmail.com>
2019-10-15 19:09:39 +0300

    Use utils.is_square()

--- python/p086.py
+++ python/p086.py
@@ -3,19 +3,21 @@
 """Problem 86: Cuboid route"""
 
 from itertools import count
-from math import hypot
+
+from utils import is_square
 
 
 def main():
     cnt = 0
     for a in count(1):
         for s in range(2, 2*a+1):
-            if hypot(a, s).is_integer():
+
+            if is_square(a*a + s*s):
                 cnt += min(s, a+1) - (s+1)//2
 
-        if cnt > 1000000:
+        if cnt > 1_000_000:
             return a
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(main())

04e3de384c174d791336594dc23ceb9b3ffacd6e
doer <e.yarmash@gmail.com>
2020-01-30 18:02:07 +0300

    Style improvements

--- python/p006.py
+++ python/p006.py
@@ -2,11 +2,13 @@
 
 """Problem 6: Sum square difference"""
 
+
 def main():
     lim = 100
-    square_of_sum = (lim*(lim+1)//2) ** 2 # arithmetic progression
+    square_of_sum = (lim*(lim+1)//2) ** 2  # arithmetic progression
     sum_of_squares = (2*lim+1)*(lim+1)*lim//6
     return square_of_sum - sum_of_squares
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     print(main())

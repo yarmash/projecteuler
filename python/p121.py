f5a18ce871a03d841b80a133a8c9674f41c08c2d
doer <e.yarmash@gmail.com>
2020-03-31 15:38:11 +0300

    Use math.prod()

--- python/p121.py
+++ python/p121.py
@@ -2,15 +2,13 @@
 
 """Problem 121: Disc game prize fund"""
 
-from functools import reduce
 from itertools import chain, combinations
-from math import factorial
-from operator import mul
+from math import factorial, prod
 
 
 def main():
     turns = 15
-    prob = sum(reduce(mul, reds, 1) for reds in chain.from_iterable(
+    prob = sum(prod(reds) for reds in chain.from_iterable(
         combinations(range(1, turns+1), n) for n in range((turns+1)//2)))
     return factorial(turns+1) // prob
 

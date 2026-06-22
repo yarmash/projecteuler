051a89a8b422a4fe2b335b65c1f5cd700faabe2b
Eugene Yarmash <e.yarmash@gmail.com>
2026-06-22 17:33:33 +0300

    Optimize the solution
    Replace manual year/month state updates with nested year and month loops

--- python/p019.py
+++ python/p019.py
@@ -2,34 +2,24 @@
 
 """Problem 19: Counting Sundays"""
 
-
-def is_leap_year(year):
-    """Determine whether a year is a leap year"""
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+from calendar import isleap as is_leap_year
 
 
 def main():
     numdays = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-
-    year, month, wday = 1901, 0, 2  # Tue, 1 Jan 1901
-    is_leap = False
+    weekday = 2  # Tuesday, 1 Jan 1901, with Sunday represented as 0
     sundays = 0
 
-    while year < 2001:
-        if wday == 0:
-            sundays += 1
-
-        days = 29 if is_leap and month == 1 else numdays[month]
-        wday = (wday + days) % 7
-
-        if month < 11:
-            month += 1
-        else:
-            month = 0
-            year += 1
-            is_leap = is_leap_year(year)
+    for year in range(1901, 2001):
+        is_leap = is_leap_year(year)
+        for month in range(12):
+            if weekday == 0:
+                sundays += 1
+            days = 29 if is_leap and month == 1 else numdays[month]
+            weekday = (weekday + days) % 7
 
     return sundays
+
 
 if __name__ == "__main__":
     print(main())

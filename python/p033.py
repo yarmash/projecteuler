8facc5522f136c91a339a5661a07be1160cfc1a2
Eugene Yarmash <e.yarmash@gmail.com>
2026-07-02 20:17:19 +0300

    Simplify the solution using fractions.Fraction

--- python/p033.py
+++ python/p033.py
@@ -2,25 +2,20 @@
 
 """Problem 33: Digit Cancelling Fractions"""
 
-from functools import reduce
-from math import gcd
+from fractions import Fraction
+from math import prod
 
 
 def main():
-    fractions = []
+    # the numbers need to be of the form AX/XB (e.g. 49/98): cancelling
+    # a shared tens or units digit forces a == b, so no other form works
+    return prod(
+        Fraction(a, b)
+        for a in range(10, 100)
+        for b in range(a + 1, 100)
+        if a % 10 == b // 10 and a * (b % 10) == b * (a // 10)  # a/b = c/d === a*d = b*c
+    ).denominator
 
-    for a in range(10, 99):
-        for b in range(a+1, 100):
-            if not a % 10 or not b % 10:
-                continue
-
-            # the numbers need to be of the form AX/XB (e.g. 49/98)
-            if a % 10 == b // 10 and a*(b % 10) == b*(a // 10): # a/b = c/d === a*d = b*c
-                fractions.append((a, b))
-
-    f = reduce(lambda x, y: (x[0]*y[0], x[1]*y[1]), fractions)
-
-    return f[1]//gcd(*f)
 
 if __name__ == "__main__":
     print(main())

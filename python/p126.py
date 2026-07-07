d9ecb047d42499531546597efc30dca002a39c9b
Eugene Yarmash <e.yarmash@gmail.com>
2026-07-08 00:51:30 +0300

    Optimize the solution using incremental layer sizes

--- python/p126.py
+++ python/p126.py
@@ -4,31 +4,30 @@
 
 
 def main():
-
-    def ncubes(x, y, z, l):
-        return 2*(x*y+x*z+y*z) + 4*(x+y+z)*(l-1) + 4*(l-1)*(l-2)
-
+    # guessed search ceiling: counts are exact for all layer sizes <= lim,
+    # so this only assumes the answer lies below it
     lim = 20000
     cn = [0]*(lim+1)
 
     for x in range(1, lim):
-        if ncubes(x, 1, 1, 1) > lim:
+        if 6*x*x > lim:  # smallest first layer for this x (y = z = x)
             break
         for y in range(x, lim):
-            if ncubes(x, y, 1, 1) > lim:
+            if 2*(2*x*y + y*y) > lim:  # smallest first layer (z = y)
                 break
             for z in range(y, lim):
-                if ncubes(x, y, z, 1) > lim:
+                c = 2*(x*y + x*z + y*z)  # first layer
+                if c > lim:
                     break
-                for n in range(1, lim):
-                    c = ncubes(x, y, z, n)
-                    if c > lim:
-                        break
-                    cn[c] += 1
 
-    for i, v in enumerate(cn):
-        if v == 1000:
-            return i
+                # layer n adds 4*(x+y+z) + 8*(n-2) cubes over layer n-1
+                d = 4*(x + y + z)
+                while c <= lim:
+                    cn[c] += 1
+                    c += d
+                    d += 8
+
+    return cn.index(1000)
 
 
 if __name__ == "__main__":
